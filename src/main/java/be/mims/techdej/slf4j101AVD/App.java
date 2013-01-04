df39cfc00dcb1a06f102e150e85174243412cebb
Arnaud Vandyck <avd@mims.be>
2013-01-04 14:41:44 +0100

    Premier log dans App

--- src/main/java/be/mims/techdej/slf4j101AVD/App.java
+++ src/main/java/be/mims/techdej/slf4j101AVD/App.java
@@ -1,13 +1,19 @@
 package be.mims.techdej.slf4j101AVD;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("App start");
+		System.out.println("Hello World!");
+		logger.info("App stop");
+	}
 }
