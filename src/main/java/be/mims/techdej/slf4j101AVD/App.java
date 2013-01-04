55c6fdd4b7689ebfa329ab999f71f1baf4e2d06e
Arnaud Vandyck <avd@mims.be>
2013-01-04 13:37:52 +0100

    maven-archetype-quickstart initial commit

---
+++ src/main/java/be/mims/techdej/slf4j101AVD/App.java
@@ -0,0 +1,13 @@
+package be.mims.techdej.slf4j101AVD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
